4d13af50ddd740cf563df886851c72e06165b1a2
Abel Tadesse <tadesseabel26@gmail.com>
2025-02-21 15:57:05 +0300

    add a programs section in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { About } from "@/components/home-ui/about";
 import Hero from "@/components/home-ui/hero";
+import { Programs } from "@/components/home-ui/programs";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
       <About />
+      <Programs />
     </main>
   );
 }
